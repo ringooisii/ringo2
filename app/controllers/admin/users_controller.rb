14f78a1ea2c10a068165e7e95a7f2c9dfc9063f9
shihosaito <s.saitosan.0611@gmail.com>
2019-02-12 18:12:24 +0900

    管理者画面のユーザー一覧ページ作りました

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,20 @@
 class Admin::UsersController < ApplicationController
+
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+		@orders = Order.all
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
 end
