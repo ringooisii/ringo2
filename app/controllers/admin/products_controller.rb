7acff93c88717b2ca99d17755ffd4d151a3dee24
能勢大輔 <nosedaisuke@nosedaisukenoMacBook-Air.local>
2019-02-13 16:35:41 +0900

    商品新規登録

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -5,9 +5,24 @@ class Admin::ProductsController < ApplicationController
 		#@song = @disc.songs.build
 	end
 
+	def index
+		@product = Product.all
+	end
+
+	def create
+		product = Product.new(product_params)
+		product.save
+		redirect_to root_path
+	end
+
+	def show
+	end
+
 	private
 		def product_params
-			params.require(:product).permit(:genre, :product_name, :price, :artist_name, :product_image, :company, :stock_quantity)
+			params.require(:product).permit(:genre, :product_name, :price, :artist_name, :product_image, :company, :stock_quantity,
+				discs_attributes: [:id, :disc_name, :disc_number, :_destroy,
+				songs_attributes: [:id, :title, :song_number, :_destroy]])
 		end
 end
 
--- app/models/disc.rb
+++ app/models/disc.rb
@@ -1,6 +1,6 @@
 class Disc < ApplicationRecord
-	#belongs_to :product
-	#has_many :song, inverse_of: :disc
-	#accepts_nested_attributes_for :song, reject_if: :all_blank, allow_destroy: true
+	belongs_to :product
+	has_many :songs, inverse_of: :disc
+	accepts_nested_attributes_for :songs, reject_if: :all_blank, allow_destroy: true
 
 end
