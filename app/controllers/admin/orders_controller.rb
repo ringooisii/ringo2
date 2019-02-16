756d8878bf82d2250006ac590be8884ef4e9af41
shihosaito <s.saitosan.0611@gmail.com>
2019-02-16 15:52:47 +0900

    管理者側の注文一覧作成中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,14 @@
 class Admin::OrdersController < ApplicationController
+
+  before_action :authenticate_user!
+
   def index
   	@orders = Order.all
+    @users = User.count
   end
 
   def show
-    @order = Order.find(parame[:id])
+     @order = Order.find(params[:id])
+     @order_products = Order_products.where(order_id: @order.id)
   end
 end
