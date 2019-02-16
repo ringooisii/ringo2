c8ab1ec5294c6ad90f58eb00a3faa8f97f8c6b00
shihosaito <s.saitosan.0611@gmail.com>
2019-02-16 10:15:22 +0900

    管理者側のユーザーページ完成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -29,7 +29,6 @@ class Admin::UsersController < ApplicationController
 		redirect_to admin_users_path
 	end
 
-
   private
   def user_params
   	params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address)
