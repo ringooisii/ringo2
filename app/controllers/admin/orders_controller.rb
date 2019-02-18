6b61f0cec51e02a1d14c335e828c63c7024aee72
sirocco1 <shino2163@gmail.com>
2019-02-18 15:58:21 +0900

    途中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,20 +7,29 @@ class Admin::OrdersController < ApplicationController
   def index
   	@orders = Order.all
     @users = User.count
-    @orders.each do |order|
-      @order_products = OrderProduct.where(order_id: order.id)
-      @order_products.each do |p|
-        @p = Product.find(p.product_id)
-      end
-    end
+    # @orders.each do |order|
+    #   @order_products = OrderProduct.where(order_id: order.id)
+    #   @order_products.each do |p|
+    #     @p = Product.find(p.product_id)
+    #   end
+    # end
+
+    @order_products = OrderProduct.all
+    @order_products.inject(0) { |sum, order_product|
+      @sales = sum + (order_product.order_quantity*order_product.product.price)
+    }
+    # @sales = 0
+    # @order_products.each do |order_product|
+    #   @sales += (order_product.order_quantity*order_product.product.price)
+    # end
   end
 
   def show
      @order = Order.find(params[:id])
      @order_products = OrderProduct.where(order_id: @order.id)
-     @order_products.each do |product|
-      @p = Product.find(product.product_id)
-    end
+    #  @order_products.each do |product|
+    #   @p = Product.find(product.product_id)
+    # end
   end
 
   def update
